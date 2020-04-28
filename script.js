d98589f44c3ffcd0f99d5dcd63fbddb3da5f909d
Mark Wilde <markwilde29@gmail.com>
2020-04-28 15:15:16 +0100

    Compared IIFEs to the new simple block of ES6.

--- script.js
+++ script.js
@@ -15,29 +15,50 @@ console.log(name6);
 // ES5
 function driversLicence5(passedTest) {
 
-if (passedTest) {
-console.log(firstName);
-var firstName = 'John';
-var yearOfBirth = 1990;
-}
+   if (passedTest) {
+      console.log(firstName);
+      var firstName = 'John';
+      var yearOfBirth = 1990;
+   }
 
 
-console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car.');
+   console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car.');
 }
 driversLicence5(true);
 // ES6
 function driversLicence6(passedTest) {
 
-//console.log(firstName);
-let firstName;
-const yearOfBirth = 1990;
+   //console.log(firstName);
+   let firstName;
+   const yearOfBirth = 1990;
 
-if (passedTest) {
-firstName = 'John';
-}
+   if (passedTest) {
+      firstName = 'John';
+   }
 
-console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car.');
+   console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car.');
 }
 driversLicence6(true);
 var i = 23;
-for (var i = 0; i < 5; i++) { console.log(i); } console.log(i); */
+for (var i = 0; i < 5; i++) {
+   console.log(i);
+}
+console.log(i);*/
+
+/////////////////////////////////
+// Lecture: Blocks and IIFEs
+
+
+// ES6
+{
+   const a = 1;
+   let b = 2;
+   var c = 3;
+}
+//console.log(a + b);
+console.log(c);
+// ES5
+(function () {
+   var c = 3;
+})();
+//console.log(c);
